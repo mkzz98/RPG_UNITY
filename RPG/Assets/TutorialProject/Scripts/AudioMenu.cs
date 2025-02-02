333b0a60e37e32931b81741c4b386da26fbe1452
Mikael <zhu1@live.se>
2025-02-02 16:36:59 +0100

    Added method to audioscript

--- RPG/Assets/TutorialProject/Scripts/AudioMenu.cs
+++ RPG/Assets/TutorialProject/Scripts/AudioMenu.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+using System;
 
 public class AudioMenu : MonoBehaviour
 {
-    
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] TMP_Text masterText, sfxText, ambienceText;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMasterVolume(float volume)
     {
-        
+        int volumeToInt = Mathf.Clamp((int)(volume * 10), 0, 20);
+        masterText.text = volumeToInt.ToString();
+        audioMixer.SetFloat("MasterVolume", MathF.Log10(volume) * 20);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        int volumeToInt = Mathf.Clamp((int)(volume * 10), 0, 20);
+        sfxText.text = volumeToInt.ToString();
+        audioMixer.SetFloat("SFXVolume", MathF.Log10(volume) * 20);
+    }
+    public void SetAmbienceVolume(float volume)
+    {
+        int volumeToInt = Mathf.Clamp((int)(volume * 10), 0, 20);
+        ambienceText.text = volumeToInt.ToString();
+        audioMixer.SetFloat("AmbianceVolume", MathF.Log10(volume) * 20);
     }
 }
